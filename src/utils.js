ec8946a7a03563013fe9879f4efc82d922d471f9
yugasun <yuga_sun@163.com>
2020-01-23 13:55:36 +0800

    feat: use @ygkit/request for wait status logical

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+const { sleep, waitStatus } = require('@ygkit/request')
 const {
   DescribeServerlessDBInstances,
   OpenServerlessDBExtranetAccess,
@@ -6,12 +7,6 @@ const {
 
 // timeout 5 minutes
 const TIMEOUT = 5 * 60 * 1000
-
-async function sleep(ms) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
-}
 
 /**
  *
@@ -60,58 +55,13 @@ async function getDbInstanceDetail(context, apig, dBInstanceName) {
  INSTANCE_STATUS_RESTARTING:  "restarting",  重启中
  */
 
-async function waitForStatus({
-  callback,
-  statusProp = 'status',
-  // wait status
-  targetStatus,
-  // timeout mini seconds
-  timeout,
-  // start mini seconds
-  start = Date.now(),
-  // promise resolve
-  resolve = null,
-  // promise reject
-  reject = null
-}) {
-  const now = Date.now()
-  return new Promise(async (res, rej) => {
-    try {
-      resolve = resolve || res
-      reject = reject || rej
-      // timeout
-      if (now - start > timeout) {
-        reject(new Error('Request Timeout.'))
-      }
-      const detail = await callback()
-      // 4: deploying, 1: created
-      if (detail && detail[statusProp] === targetStatus) {
-        resolve(detail)
-      } else {
-        await sleep(1000)
-        return waitForStatus({
-          callback,
-          statusProp,
-          targetStatus,
-          timeout,
-          start,
-          resolve,
-          reject
-        })
-      }
-    } catch (e) {
-      reject(e)
-    }
-  })
-}
-
 async function toggleDbInstanceAccess(context, apig, dBInstanceName, extranetAccess) {
   if (extranetAccess) {
     context.debug(`Start open db extranet access...`)
     await OpenServerlessDBExtranetAccess(apig, {
       DBInstanceName: dBInstanceName
     })
-    const detail = await waitForStatus({
+    const detail = await waitStatus({
       callback: async () => getDbInstanceDetail(context, apig, dBInstanceName),
       targetStatus: 'running',
       statusProp: 'DBInstanceStatus',
@@ -124,7 +74,7 @@ async function toggleDbInstanceAccess(context, apig, dBInstanceName, extranetAcc
   await CloseServerlessDBExtranetAccess(apig, {
     DBInstanceName: dBInstanceName
   })
-  const detail = await waitForStatus({
+  const detail = await waitStatus({
     callback: async () => getDbInstanceDetail(context, apig, dBInstanceName),
     targetStatus: 'running',
     statusProp: 'DBInstanceStatus',
@@ -142,7 +92,7 @@ function formatPgUrl(netInfo, accountInfo, dbName) {
 module.exports = {
   TIMEOUT,
   getDbInstanceDetail,
-  waitForStatus,
+  waitStatus,
   toggleDbInstanceAccess,
   formatPgUrl,
   sleep
