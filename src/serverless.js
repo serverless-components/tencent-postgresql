4127111bf8839766d3d1f57d17434a473a3a5caf
yugasun <yuga_sun@163.com>
2020-04-16 17:38:37 +0800

    fix: throw error when no temp secrets

--- src/serverless.js
+++ src/serverless.js
@@ -3,16 +3,26 @@ const { Postgresql } = require('tencent-component-toolkit')
 const { prepareInputs } = require('./utils')
 
 class PgComponent extends Component {
-  async deploy(inputs) {
-    console.log(`Deploying PostgreSQL Database...`)
-
-    // 获取腾讯云密钥信息
+  getCredentials() {
     const { tmpSecrets } = this.credentials.tencent
-    const credentials = {
+
+    if (!tmpSecrets || !tmpSecrets.TmpSecretId) {
+      throw new Error(
+        'Cannot get secretId/Key, your account could be sub-account or does not have access, please check if SLS_QcsRole role exists in your account, and visit https://console.cloud.tencent.com/cam to bind this role to your account.'
+      )
+    }
+
+    return {
       SecretId: tmpSecrets.TmpSecretId,
       SecretKey: tmpSecrets.TmpSecretKey,
       Token: tmpSecrets.Token
     }
+  }
+
+  async deploy(inputs) {
+    console.log(`Deploying PostgreSQL Database...`)
+
+    const credentials = this.getCredentials()
 
     // 对Inputs内容进行标准化
     const pgInputs = await prepareInputs(inputs)
@@ -33,12 +43,9 @@ class PgComponent extends Component {
     console.log(`Removing PostgreSQL Database...`)
 
     const { state } = this
-    const { tmpSecrets } = this.credentials.tencent
-    const credentials = {
-      SecretId: tmpSecrets.TmpSecretId,
-      SecretKey: tmpSecrets.TmpSecretKey,
-      Token: tmpSecrets.Token
-    }
+
+    const credentials = this.getCredentials()
+
     const pgBaas = new Postgresql(credentials, state.region)
 
     await pgBaas.remove({
